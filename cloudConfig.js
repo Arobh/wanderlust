30d50dd9db4629b2b3bd17e1da7e51849a20da36
Arobh Kumar <aarobhs087@gmail.com>
2025-02-11 22:45:12 +0530

    few change commit message

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "Wonderlust_DEV",
+    folder: "Arobnb_DEV",
     format: async (req, file) => {
       // Check file extension and return format accordingly
       const fileExtension = file.originalname.split(".").pop();
